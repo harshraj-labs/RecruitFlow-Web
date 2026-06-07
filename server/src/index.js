7c805b2330a0e37d533d3a9ffe9af8253bfdc0db
Harsh Raj <harshraj.contact@gmail.com>
2026-06-07 13:20:40 +0530

    Fix multer version for railway deployment

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
     origin: [
-        'http://localhost:5173' //local host
+        'http://localhost:5173', //local host
         process.env.FRONTEND_URL
         
     ],
